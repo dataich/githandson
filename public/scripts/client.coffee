cda47b7cddb9dbdb2515262591d9f6ab7ffb92f4
Taichiro Yoshida <taichiro.yoshida@gmail.com>
2011-06-04 00:44:40 +0900

    更新時にサウンドを再生するようにした

--- public/scripts/client.coffee
+++ public/scripts/client.coffee
@@ -1,3 +1,9 @@
+audio = new Audio '/public/sounds/sound.mp3';
+audio.controls = false
+audio.autoplay = false
+audio.preload = 'auto'
+audio.autobuffler = true
+
 $(document).ready () ->
   socket = new io.Socket null
   socket.connect()
@@ -6,6 +12,7 @@ $(document).ready () ->
 #    alert 'connect'
     
   socket.on 'message', (message) ->
+    audio.play()
     $('#content').html message
     
 #  socket.on 'disconnect', ->
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,16 @@
 (function() {
+  var audio;
+  audio = new Audio('/public/sounds/sound.mp3');
+  audio.controls = false;
+  audio.autoplay = false;
+  audio.preload = 'auto';
+  audio.autobuffler = true;
   $(document).ready(function() {
     var socket;
     socket = new io.Socket(null);
     socket.connect();
     return socket.on('message', function(message) {
+      audio.play();
       return $('#content').html(message);
     });
   });
